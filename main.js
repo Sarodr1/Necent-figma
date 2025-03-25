fe3771b8759c12c9102c524d76b823cad5437b2e
Sarodr1 <sardorismatullaev817@gmail.com>
2025-03-25 20:42:28 +0500

    Kod yangilandi

--- main.js
+++ main.js
@@ -14,3 +14,46 @@
 
 // 2 misol
 
+// function yigindi(...nums) {
+//     return nums.reduce((sum, num) => sum + num, 0);
+// }
+
+// console.log(yigindi(1, 2, 3));      // 6
+// console.log(yigindi(5, 10, 15, 20)); // 50
+
+
+
+// 3 misol 
+
+// const user = { name: "Sardor", age: 15 };
+// const { name, age } = user;
+// console.log(name); // "Sardor"
+// console.log(age);  // 15
+
+
+// 4 misol 
+
+// function objectToArray(obj) {
+//     return Object.entries(obj)
+// }
+
+// console.log(JSON.stringify(objectToArray({ name: "Ali", age: 25 })));
+
+
+// 5 misol 
+
+// const getAndValues = (obj) => {
+//     return Object.keys(obj)
+// }
+
+// console.log(getAndValues({ a: 1, b: 2, c: 3}));
+
+
+
+// 6 misol
+
+
+// const sumPositiveValues = (obj) => {
+//     return Object.values(obj).reduce((sum, num) => num > 0 ? sum + num : sum, 0);
+// }
+// console.log(sumPositiveValues({ a: -5, b: 10, c: 15 }));    
