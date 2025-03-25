ea36facd304d806143fd21045a6dcb7e53b18646
Sarodr1 <sardorismatullaev817@gmail.com>
2025-03-25 15:53:45 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+// 1 misol
+
+// function createUser(name, age) {
+//     return new Object({
+//         name: name,
+//         age: age,
+//         isAdmin: false
+//     })
+// }
+
+// console.log(createUser("Sardor", 15));
+
+
+
+// 2 misol
+
